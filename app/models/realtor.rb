673b6668b14498e718d45d20478d49a37eea9399
Marin955 <marin.bilic55@gmail.com>
2024-02-15 19:00:41 +0100

    added search functionality, styled a bit better, removed unnecessary code

--- app/models/realtor.rb
+++ app/models/realtor.rb
@@ -1,2 +1,9 @@
 class Realtor < ApplicationRecord
+  def self.search_by_first_name(first_name)
+    if first_name.present?
+      where("first_name ILIKE ?", "%#{first_name}%")
+    else
+      all
+    end
+  end
 end
